1f7cbf1f163e233c21494b9e532d5de6ee555542
Bill Ton Hoang Nguyen <bill.nguyentonhoang@live.com>
2022-02-24 21:58:42 +0100

    A* search implemented

--- src/assignment_1/Sokoban4J/src/MyAgent.java
+++ src/assignment_1/Sokoban4J/src/MyAgent.java
@@ -1,6 +1,7 @@
 import static java.lang.System.out;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
 
 import agents.ArtificialAgent;
 import game.actions.EDirection;
@@ -22,7 +23,7 @@ public class MyAgent extends ArtificialAgent {
 		long searchStartMillis = System.currentTimeMillis();
 		
 		List<EDirection> result = new ArrayList<EDirection>();
-		dfs(5, result); // the number marks how deep we will search (the longest plan we will consider)
+		//dfs(5, result); // the number marks how deep we will search (the longest plan we will consider)
 
 		long searchTime = System.currentTimeMillis() - searchStartMillis;
         
@@ -33,6 +34,48 @@ public class MyAgent extends ArtificialAgent {
         }
 		
 		return result.isEmpty() ? null : result;
+	}
+	
+	private int heuristic_function(BoardCompact state) {
+		return 0;
+	}
+
+  	private boolean search(List<EDirection> result) {
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		Set<BoardCompact> explored = new HashSet<>();
+		BoardCompact init = board.clone();
+		
+		Node current = new Node(init, null, null, 0, heuristic_function(init));
+
+		pq.add(current);
+		explored.add(init);
+
+		do {
+			searchedNodes++;
+
+			current = pq.remove(); 
+			BoardCompact currState = current.getState();
+
+			List<CAction> actions = new LinkedList<>();
+			
+			for (CMove move : CMove.getActions()) {
+				if (move.isPossible(board)) actions.add(move);
+			}
+			for (CPush push : CPush.getActions()) {
+				if (push.isPossible(board)) actions.add(push);
+			}
+			
+			for (CAction action : action) {
+				BoardCompact nextState  = action.perform(board.clone());
+				if (explored.contains(nextState)) continue;
+				explored.add(nextState);
+				pq.add(new Node<S,A>(nextState, action, current, current.getCost() + 1, heuristic_function(nextState)));
+			}
+
+		} while (!pq.isEmpty() && !current.getState().isVictory());
+
+		current.reconstructPath(result);
+		return current.getState().isVictory();
 	}
 
 	private boolean dfs(int level, List<EDirection> result) {
@@ -79,5 +122,39 @@ public class MyAgent extends ArtificialAgent {
 		}
 		
 		return false;
+	}
+}
+
+class Node implements Comparable<Node> {
+	private BoardCompact state;
+	private double h;
+	private double cost;
+	public CAction incomingAction;
+	public Node pred;
+
+	public Node(BoardCompact state, A action, Node pred, double cost, double estimate) {
+		this.state = state;
+		this.h = estimate;
+		this.incomingAction = action;
+		this.cost = cost;
+		this.pred = pred;
+	}
+
+	public BoardCompact getState() { return state; }
+
+	public double getCost() { return cost; }
+
+	public void reconstructPath(List<EDirection> actions) {
+		Node curr = this;
+		while (curr != null && curr.incomingAction != null) {
+			actions.add(curr.incomingAction.getDirection());
+			curr = curr.pred;
+		}
+		Collections.reverse(actions);
+	}
+
+	@Override
+	public int compareTo(Node other) {
+		return Double.compare(this.h + this.cost, other.h + other.cost);
 	}
 }
